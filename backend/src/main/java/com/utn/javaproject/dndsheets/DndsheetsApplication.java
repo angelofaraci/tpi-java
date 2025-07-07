b286525541626e85239b6ab8d6d57f28653c27b3
angelofaraci <angelofaraci089@yahoo.com>
2025-07-07 10:14:42 -0300

    intial commit

---
+++ backend/src/main/java/com/utn/javaproject/dndsheets/DndsheetsApplication.java
@@ -0,0 +1,13 @@
+package com.utn.javaproject.dndsheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndsheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndsheetsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/utn/javaproject/dndsheets/DndsheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.javaproject.dndsheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndsheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
